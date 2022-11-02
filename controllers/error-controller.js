54cdb32f43956347b9393a4a5d76bc295e0d643f
Moaz Mohamed Hassan Bayoumi <moaz25jan2015@gmail.com>
2022-11-03 00:47:12 +0200

    Removing rendering error views

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -22,21 +22,14 @@ const handleJWTError = () =>
 const handleTokenExpiredError = () =>
   new AppError("Token is expired! Please log in again!", 401);
 const sendErrorDev = (req, res, err) => {
-  if (req.originalUrl.startsWith("/api")) {
     return res.status(err.statusCode).json({
       status: err.status,
       error: err,
       message: err.message,
       stack: err.stack,
     });
-  }
-  return res.status(err.statusCode).render("error", {
-    title: "Something went wrong!",
-    msg: err.message,
-  });
 };
 const sendErrorProd = (req, res, err) => {
-  if (req.originalUrl.startsWith("/api")) {
     if (err.isOperational) {
       return res.status(err.statusCode).json({
         status: err.status,
@@ -48,18 +41,6 @@ const sendErrorProd = (req, res, err) => {
       status: "error",
       message: "Something went very wrong!",
     });
-  }
-  if (err.isOperational) {
-    return res.status(err.statusCode).render("error", {
-      title: "Something went wrong!",
-      msg: err.message,
-    });
-  }
-  console.error("ERROR 💥", err);
-  return res.status(err.statusCode).render("error", {
-    title: "Something went wrong!",
-    msg: "Please try again later.",
-  });
 };
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
