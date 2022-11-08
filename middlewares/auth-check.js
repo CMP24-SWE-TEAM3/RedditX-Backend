5527df113a98939e4214c0150dea4a2cd84a2b0f
Ahmed Lotfy <76037906+AhmedLotfy02@users.noreply.github.com>
2022-11-08 12:59:19 +0200

    (update): decode token
    send the username decoded from the token to request

--- middlewares/auth-check.js
+++ middlewares/auth-check.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, "mozaisSoHotButNabilisTheHottest");
+        const decoded =jwt.verify(token, "mozaisSoHotButNabilisTheHottest");
+        req.username=decoded.username;
         next();
     } catch (error) {
         res.status(401).json({ message: "Auth failed!" });
