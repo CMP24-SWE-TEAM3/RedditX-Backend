9308c0ef6f3e50fb692adb54a4c52f9304af53a5
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2022-11-11 20:01:48 +0200

    Adding checking of available email in sign up

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -6,5 +6,4 @@ const router = express.Router();
 router.get("/username-available/", authController.availableUsername);
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
-router.get("/get-user-prefs/:username", authController.getUserPrefs);
 module.exports = router;
