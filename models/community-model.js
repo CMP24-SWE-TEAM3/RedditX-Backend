c94d5e9c2263d04855813c0a54d082481f10bed9
Ahmed Lotfy <76037906+AhmedLotfy02@users.noreply.github.com>
2022-11-11 21:45:32 +0200

    documentations are done

--- models/community-model.js
+++ models/community-model.js
@@ -47,7 +47,7 @@ const communityOptionsSchema = mongoose.Schema({
   {
     type: String,
     default:"top"
-  },
+  },  
   postType: Number, // 0 any, 1 videos and images only, and 2 text only
   region: String,
   privacyType: String, // "public" (anyone can view and submit), "private" (only approved members can view and submit), or "restricted" (anyone can view, but only some are approved to submit links)
