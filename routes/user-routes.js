708971e49a0ed531e5e001f2cd9247f58260a2fd
mohamednabilabdelfattah <76039904+mohamednabilabdelfattah@users.noreply.github.com>
2022-12-13 17:54:48 +0200

    add general update api

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -19,9 +19,9 @@ router.get("/me/prefs", authCheck, userController.getUserPrefs);
 router.get("/me", authCheck, userController.getUserMe);
 
 router.get("/:username/about", userController.getUserAbout);
-router.get("/me/followers",authCheck, userController.followers);
-router.get("/me/interests",authCheck, userController.getInterests);
-router.post("/me/interests",authCheck, userController.addInterests);
+router.get("/me/followers", authCheck, userController.followers);
+router.get("/me/interests", authCheck, userController.getInterests);
+router.post("/me/interests", authCheck, userController.addInterests);
 
 router.post("/block-user", authCheck, userController.block);
 router.post("/spam", authCheck, userController.spam);
@@ -40,6 +40,7 @@ router.get("/userCommentReplies/:username", profileController.getUserCommentRepl
 router.get("/userSelfReply/:username", profileController.getUserSelfReply);
 
 
-router.post("/subscribe",authCheck,userController.subscribe);
+router.post("/subscribe", authCheck, userController.subscribe);
+router.post('/update', authCheck, userController.updateInfo);
 
 module.exports = router;
